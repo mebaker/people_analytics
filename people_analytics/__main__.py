5ae068f6c775f8c0bb30cad291b71503d162aa2f
Marcus Baker <marcusedwardbaker@gmail.com>
2020-12-05 21:15:23 -0500

    fix: unit tests

--- people_analytics/__main__.py
+++ people_analytics/__main__.py
@@ -3,13 +3,18 @@ import argparse
 from datetime import datetime
 from .tasks.output_clean_data import OutputCleanData
 from .tasks.site_metrics import SiteMetrics
+
+default = datetime.now().strftime("%Y-%m-%d")
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--date", default=datetime.now().strftime("%Y-%m-%d"))
+parser.add_argument("-d", "--date", default=default)
 
 
 def main(*args):
-    args = parser.parse_args(*args)
-    date = str(args.date).replace("/", "-")
+    if args:
+        args = parser.parse_args(*args)
+        date = str(args.date).replace("/", "-")
+    else:
+        date = default
     build(
         [OutputCleanData(report_date=date), SiteMetrics(report_date=date)],
         local_scheduler=True,
--- people_analytics/models/test/test_employee_data.py
+++ people_analytics/models/test/test_employee_data.py
@@ -5,9 +5,9 @@ from ..employee_data import EmployeeData
 
 class EmployeeDataTest(TestCase):
     def test_init(self):
-        data = pd.read_csv("./test/data/input/employee_data.csv", na_filter=False).to_dict(
-            "records"
-        )
+        data = pd.read_csv(
+            "./test/data/input/employee_data.csv", na_filter=False
+        ).to_dict("records")
         data = list(map(lambda x: EmployeeData(x), data))
         self.assertTrue(isinstance(data[0], EmployeeData))
 
